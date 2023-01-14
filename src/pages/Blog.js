0b30cc51fa91cd981aaf6b53696d157efc76b53f
Tuan Anh <tutuananh975@gmail.com>
2023-01-14 14:07:20 +0700

    web

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import BlogBody from "../components/BlogBody";
 import BlogFooter from "../components/BlogFooter";
 import BLogHeader from "../components/BlogHeader";
@@ -5,12 +6,40 @@ import '../css/blog.css'
 
 
 function Blog() {
+    const [scrollY, setScrollY] = useState(() => {
+        return window.scrollY
+    });
+    const [scrollTop, setScrollTop] = useState(false)
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrollY(window.scrollY);
+            
+            setScrollTop(window.scrollY > 0 && scrollY > window.scrollY)
+        }
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [scrollY])
+
+    const handleScrollToTop = () => {
+        window.scroll(0,0)
+    }
+
+    console.log('re-render')
 
     return (
         <div className="blog-page">
             <BLogHeader></BLogHeader>
             <BlogBody></BlogBody>
             <BlogFooter></BlogFooter>
+            {scrollTop && (
+                <button
+                    className="position-fixed bottom-0 end-0"
+                    onClick={handleScrollToTop}
+                >
+                    Go to top
+                </button>
+            )}
         </div>
     )
 }
